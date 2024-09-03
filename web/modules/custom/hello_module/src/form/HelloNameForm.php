dc956dba76ec2f380591ed45672c0e8039b900a7
Alex Tran <tranduclong092@gmail.com>
2024-09-03 12:40:40 +0300

    Feat: add a greeting page and modify the main controller and the form

--- web/modules/custom/hello_module/src/form/HelloNameForm.php
+++ web/modules/custom/hello_module/src/form/HelloNameForm.php
@@ -9,7 +9,7 @@ class HelloNameForm extends FormBase
 {
   public function getFormId()
   {
-    return 'hello_name_form';
+    return 'hello-form';
   }
 
   public function buildForm(array $form, FormStateInterface $form_state)
@@ -32,5 +32,6 @@ class HelloNameForm extends FormBase
   {
     $name = $form_state->getValue('name');
     \Drupal::messenger()->addMessage($this->t('Hello, @name!', ['@name' => $name]));
+    $form_state->setRedirect('hello_module.greeting', ['user' => $name]);
   }
 }
